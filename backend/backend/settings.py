85b485bd63b2631ba0272809c198317fbbbd7b81
Hilarion <102991768+Hilarion13225@users.noreply.github.com>
2026-06-17 01:47:52 +0000

    fix bug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,8 +139,7 @@ STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # adresse de React
-    "https://crud-users-phi.vercel.app",
-    "https://crud-users-g73jg6jx4-konans-projects-d411ba30.vercel.app/",# à ajouter après déploiement Vercel
+    "https://crud-users-phi.vercel.app/",  # à ajouter après déploiement Vercel
 ]
 
 # Pour servir les fichiers statiques
